bd6404943db30f61cd2d057ae12209d0df630d8d
galanteria01 <shanuu12e@gmail.com>
2020-09-13 20:48:37 +0530

    WIP

--- script1.js
+++ script1.js
@@ -1,4 +1,15 @@
 
 function them(val){
-	document.getElementById("edu").value += val
+	document.getElementById("result").value += val;
+	return false;
+}
+
+function solve(){
+	let x = document.getElementById('result').value;
+	let y = eval(x);
+	document.getElementById('result').value = y;
+}
+
+function clr(){
+	document.getElementById("result") = "";
 }
