90897a2c2413745dfa9441be2409fb910ac67c34
shanuu <shanuu12e@gmail.com>
2020-11-04 12:46:25 +0530

    React done in it

--- src/Calc.js
+++ src/Calc.js
@@ -6,20 +6,41 @@ class Calc extends Component{
         super(props);
         this.state = {
             current: '',
-            previous: []
+            previous: [],
+            nextIsReset: false
         }
 
     }
     reset = () => {
-        this.setState({result: 0});
+        this.setState({current: '0',previous:[], nextIsReset:false});
 
     }
     addToCurrent = (symbol) =>{
-        this.setState({current: this.state.current+symbol})
+        if(["/","-","+","*"].indexOf(symbol)>-1){
+            let {previous} = this.state
+            previous.push(this.state.current + symbol);
+            this.setState({previous,nextIsReset:true});
+        }else{
+            if((this.state.current === "0" && symbol !== ".") || this.state.nextIsReset)
+            {
+                this.setState({current: symbol,nextIsReset: false})
+
+            }else{
+                this.setState({current: this.state.current+symbol})
+            }
+        }
 
     }
-    equals = () =>{
-        
+    equals = (symbol) =>{
+        let {current,previous,nextIsReset} = this.state;
+        if(previous.length>0){
+            current = eval(String(previous[previous.length-1] + current));
+            this.setState({current,previous,nextIsReset:true});
+
+
+        }
+
+
     }
 
     
@@ -31,7 +52,7 @@ class Calc extends Component{
             {symbol: '7',cols: 1, action: this.addToCurrent},
             {symbol: '8',cols: 1, action: this.addToCurrent},
             {symbol: '9',cols: 1, action: this.addToCurrent},
-            {symbol: 'X',cols: 1, action: this.addToCurrent},
+            {symbol: '*',cols: 1, action: this.addToCurrent},
             {symbol: '4',cols: 1, action: this.addToCurrent},
             {symbol: '5',cols: 1, action: this.addToCurrent},
             {symbol: '6',cols: 1, action: this.addToCurrent},
@@ -46,6 +67,9 @@ class Calc extends Component{
         ];
         return( 
         <div className="Calc">
+            {this.state.previous.length > 0 ?
+            <div className="floaty-last">{this.state.previous[this.state.previous.length-1]}</div>
+        :null}
             <input className="result" type="text" value={this.state.current}/>
             <br/>
             {buttons.map((btn,i) => {
